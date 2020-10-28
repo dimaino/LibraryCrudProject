d9b8891820a9574bb54058a05e405eca54225e9a
bbogati <bimala_bogati@hotmail.com>
2020-10-27 17:41:04 -0700

    working allcurrentcheckout and added total of 12 functions , lets keep naming convention consistent :)

--- src/main/java/com/cognixia/jump/dao/BookCheckoutDao.java
+++ src/main/java/com/cognixia/jump/dao/BookCheckoutDao.java
@@ -1,14 +1,73 @@
 package com.cognixia.jump.dao;
 
+import java.sql.Date;
 import java.util.List;
 
 import com.cognixia.jump.model.BookCheckout;
 
-public interface BookCheckoutDao {
 
-	public List<BookCheckout> getAllBookCheckouts();
-	public BookCheckout getBookCheckoutById(int id);
+/*
+ * 
+ * 	private int checkout_id;
+	private int patron_id;
+	private String isbn;
+	private Date checkedout;
+	private Date due_date;
+	private Date returned;
+ *
+ *Total of 12 functions 
+ */
+
+public interface BookCheckoutDao {
+	
+	//all the book in the library that has been currently checked out 
+	// make sure the due_date is in future and returned = false 
+	//using and because some people return before due date 
+	//if we don't check returned than we might say its checked out but has already been returned
+	public List<BookCheckout> getAllCurrentBookCheckouts();
+	
+	
+	//all the books that had been checked out in the past
+	// make sure that the due_date is in the past and returned =true
+	//both conditions must be true because having a due date does not guarantee return of book
+	//book was checked out in the past but not right now , so its available 
+	public List<BookCheckout> getAllPastBookCheckouts();
+	
+	
+	//to check how many books were checked out before certain date 
+	public List<BookCheckout> getAllBookCheckoutsByDate( Date date);
+	
+	//to check how many books were checked out between certain dates 
+	public List<BookCheckout> getAllBookCheckoutsBetweenDate( Date date1, Date date2);
+	
+	//-------------------------------------------------------------------------------------//
+	
+	//gets all the current books checked out by patron id 
+	//public List<BookCheckout> getAllCurrentCheckoutsByPatronId(int patronID, Date date);
+	public List<BookCheckout> getAllCurrentCheckoutsByPatronId(int patronID);
+
+	
+	public List<BookCheckout> getAllPastCheckoutsByPatronId(int patronID);
+	
+	public List<BookCheckout> getAllCurrentBookCheckoutsByISBN( String ISBN);
+	
+	public List<BookCheckout> getAllPastBookCheckoutsByISBN( String ISBN);
+	
+	//-------------------------------------------------------------------------------------//
+	
+	
+	BookCheckout getBookCheckoutByCheckoutId(int id);
+	
+	
+	
+	//add the currently checked out book to the BookCheckout table 
 	public boolean addBookCheckout(BookCheckout bookCheckout);
+	
+	////delete the currently checked out book to the BookCheckout table 
 	public boolean deleteBookCheckout(int id);
+	
+	//BookCheckout table should not be allowed to update !!!
 	public boolean updateBookCheckout(BookCheckout bookCheckout);
+
+	
 }
--- src/main/java/com/cognixia/jump/web/LibrarianServlet.java
+++ src/main/java/com/cognixia/jump/web/LibrarianServlet.java
@@ -1,16 +1,27 @@
 package com.cognixia.jump.web;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.cognixia.jump.connection.ConnectionManager;
+import com.cognixia.jump.dao.BookCheckoutDaoImp;
+import com.cognixia.jump.model.BookCheckout;
+
 
 /**
  * Servlet implementation class LibrarianServlet
  */
 public class LibrarianServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private BookCheckoutDaoImp bookCheckoutDaoImp;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -19,6 +30,24 @@ public class LibrarianServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    @Override
+	public void init() {
+		
+    	bookCheckoutDaoImp = new BookCheckoutDaoImp();
+	}
+	
+	@Override
+	public void destroy() {
+		
+		try {
+			ConnectionManager.getConnection().close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
     
     /**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -43,22 +72,52 @@ public class LibrarianServlet extends HttpServlet {
 		
 		switch(action) {
 		
-			case("/list"):
-				listBookCheckouts(request,response);
+			case("/listCurrentBookCheckouts"):
+				getAllCurrentBookCheckouts(request,response);
+			break;
+			case("/listPastBookCheckouts"):
+				getAllPastBookCheckouts(request,response);
+			break;
+			default:
+				response.sendRedirect("/");
+				break;
 				
 				
 		
 		
 		
-		}
+		} //end of switch 
 		
+		
+		
+	} //end of doGet()
+	
+	private void getAllCurrentBookCheckouts(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+
+		List<BookCheckout> currentCheckedOutBooks = bookCheckoutDaoImp.getAllCurrentBookCheckouts();
+		System.out.println("called getAllCurrentBookCheckouts in LibarianServlet, currentCheckedOutBooks = " + currentCheckedOutBooks);
+		
+		request.setAttribute("currentCheckedOutBooks", currentCheckedOutBooks);
+		
+		//RequestDispatcher dispatcher = request.getRequestDispatcher("BookCheckout-list.jsp");
+		System.out.println("sent");
+		System.out.println("this is the request being sent"+request.getServerName()+request.getLocalPort());
+		System.out.println("this is the response being sent"+response.toString());
 		
 		
 	}
-	
-	private void listBookCheckouts(HttpServletRequest request, HttpServletResponse response) 
+	private void getAllPastBookCheckouts(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		//call the BookCheckoutDaoImp.java list function 
+		List<BookCheckout> pastCheckedOutBooks = bookCheckoutDaoImp.getAllPastBookCheckouts();
+		System.out.println("called getAllPastBookCheckouts in LibarianServlet, pastCheckedOutBooks = " + pastCheckedOutBooks);
+		
+		request.setAttribute("currentCheckedOutBooks", pastCheckedOutBooks);
+		
+		//RequestDispatcher dispatcher = request.getRequestDispatcher("BookCheckout-list.jsp");
+		System.out.println("sent");
+		System.out.println("this is the request being sent"+request.getServerName()+request.getLocalPort());
+		System.out.println("this is the response being sent"+response.toString()); 
 		
 		
 	}
